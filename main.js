3e7f547b3ea8712969d6959f48da433f55422b8f
Agusgoi <agus.goitea@live.com.ar>
2022-11-05 03:17:25 -0300

    filters

--- main.js
+++ main.js
@@ -2,72 +2,72 @@
 
 const $ = (selector) => document.querySelector (selector);
 
-// BOTONES HEADER
+
+// CAMBIO DE MODE
 
 // ----- Variables -----
 
-let btnMode = $ ('.cont-btn-mode #mode');
+let btnMode = $ ('.mode');
+let spanMode = $ ('.span-mode');
 let header = $ ('header');
-let btnHeader = $ ('#mode', '#btn-image', '#btn-text'); // no me funciona de  avarios
-let section = $ ('section');
-let asideText = $ ('#txt-editor');
-let asideImage = $ ('#img-editor');
-let textarea = $ ('#top-txt', '#bottom-txt'); 
-let btnText = $ ('#btn-text');
-let btnImage = $ ('#btn-image');
+let section = $ ('.section-meme');
+let asideText = $ ('.txt-aside');
+let asideImage = $ ('.img-aside');
+let btnText = $ ('.btn-text');
+let btnImage = $ ('.btn-image');
+let inputColors = $ ('.cont-color');
 
-//CAMBIO DE MODE - Eventos
+
+
+
+// ----- Eventos -----
+
 
 btnMode.addEventListener ("click", (event)=> {
-    header.classList.toggle ("darkmode-header");
-    let estaModoOscuro = header.classList.contains ("darkmode-header");
+    header.classList.toggle("darkmode-header");
+    let estaModoOscuro = header.classList.contains("darkmode-header");
     if (estaModoOscuro) {
-    btnMode.span.innerText = "Modo Oscuro"
+    spanMode.innerText = "Modo Oscuro";
     }else{
-    btnMode.span.innerText = "Modo Claro"
+    spanMode.innerText = "Modo Claro";
 }
 })
 
+
 btnMode.addEventListener ("click", (event)=> {
+    btnMode.classList.toggle ("darkmode-header");
+    btnText.classList.toggle ("darkmode-header");
+    btnImage.classList.toggle ("darkmode-header");
     section.classList.toggle ("darkmode-back");
-})
-
-//NO SE ME APLICA EL CAMBIO DE COLOR DEL BACKGROUND
-btnMode.addEventListener ("click", (event)=> {
-    btnMode.classList.toggle ("darkmode-btn");
-})
-
-
-//NO SE ME APLICA EL CAMBIO DE COLOR DEL BACKGROUND
-btnMode.addEventListener ("click", (event)=> {
     asideText.classList.toggle ("darkmode-aside");
-    
+    asideImage.classList.toggle ("darkmode-aside");
+    topTextImput.classList.toggle ("darkmode-header");
+    bottomTextImput.classList.toggle ("darkmode-header");
+    fontSelect.classList.toggle ("darkmode-header");
+    leftAlign.classList.toggle ("darkmode-header");
+    centerAlign.classList.toggle ("darkmode-header");
+    rightAlign.classList.toggle ("darkmode-header");
+    fontSize.classList.toggle ("darkmode-header");
+    inputColors.classList.toggle ("darkmode-header");
 })
 
-btnMode.addEventListener ("click", (event)=> {
-    textarea.classList.toggle ("darkmode-header");
-    
-})
 
 
 
-//  BOTONES PARA EDITORES
+//  Eventos de BTN TEXT & IMAGE
 
 
-//  no se me aplica el display none
 btnText.addEventListener ("click", (event)=>{
    asideText.classList.remove ("display-none");
    asideImage.classList.add ("display-none");
-    console.log (event);
     
 })
 
 btnImage.addEventListener ("click", (event)=>{
     asideText.classList.add ("display-none");
     asideImage.classList.remove ("display-none");
-     console.log (event);
      
- })
+})
 
 
 
@@ -78,25 +78,30 @@ btnImage.addEventListener ("click", (event)=>{
 // ------ Variables -------
 
 let body = document.body;
-let bottomTextMeme = $ ('.text-2');
-let bottomTextImput = $ ('#bottom-txt');
-let topTextMeme = $ ('.text-1');
-let topTextImput = $ ('#top-txt');
+
+
+let topTextImput = $ ('.input-textarea1');
+let bottomTextImput = $ ('.input-textarea2');
+let textSup = $ ('.text-1');
+let textInf = $ ('.text-2');
+
 let checkboxTop = $ ('#checkbox-top');
 let checkboxBottom = $ ('#checkbox-bottom');
 let checkboxBack = $ ('#checkbox-back');
-let textSup = $ ("#textOne");
-let textInf = $ ("#textTwo");
+
 let fontSize = $ ('.font-size');
-let fontSelect = $ ('#font-type');
-let leftAlign = $ ('#left-align');
-let centerAlign = $ ('#center-align');
-let rightAlign = $ ('#right-align');
-let noneOut = $ ('#none-out');
-let whiteOut = $ ('#white-out');
-let blackOut = $ ('#black-out');
+let fontSelect = $ ('.input-font');
+
+let leftAlign = $ ('.btn-align1');
+let centerAlign = $ ('.btn-align2');
+let rightAlign = $ ('.btn-align3');
+
+let noneOut = $ ('.btn-outline1');
+let whiteOut = $ ('.btn-outline2');
+let blackOut = $ ('.btn-outline3');
+
 let fontColor = $ ('.input-color-font');
-let fontColorBack = $ ('.input-color-back');
+let textBackColor = $ ('.input-color-back');
 let nameColor = $ ('.name-color');
 let nameColorBack = $ ('.name-color-back');
 let spacing = $ ('.input-space');
@@ -106,16 +111,16 @@ let lineSpacing = $ ('#line-spacing');
 let contImage = $ ("#meme-container");
 
 
-// TEXT EDITOR ASIDE ----- Eventos ------
+// ----- Eventos ------
 
 
 bottomTextImput.addEventListener ("input", (event)=>{
-    bottomTextMeme.innerText = event.target.value;
+    textInf.innerText = event.target.value;
 
 })
 
 topTextImput.addEventListener ("input", (event)=>{
-    topTextMeme.innerText = event.target.value;
+    textSup.innerText = event.target.value;
    
 })
 
@@ -147,116 +152,108 @@ checkboxBack.addEventListener ("change", (event)=>{
 
 
 fontSize.addEventListener ("input", (event)=>{
-    bottomTextMeme.style.fontSize = `${event.target.value}px`;
-    topTextMeme.style.fontSize = `${event.target.value}px`;
+    textSup.style.fontSize = `${event.target.value}px`;
+    textInf.style.fontSize = `${event.target.value}px`;
 })
 
 
 fontSelect.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.fontFamily = `${event.target.value}`;
-    topTextMeme.style.fontFamily = `${event.target.value}`;
+    textSup.style.fontFamily = `${event.target.value}`;
+    textInf.style.fontFamily = `${event.target.value}`;
   
 })
   
 leftAlign.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.justifyContent = 'left';
-    topTextMeme.style.justifyContent = 'left';
+    textSup.style.justifyContent = 'left';
+    textInf.style.justifyContent = 'left';
 })
 
 centerAlign.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.justifyContent = 'center';
-    topTextMeme.style.justifyContent = 'center';
+    textSup.style.justifyContent = 'center';
+    textInf.style.justifyContent = 'center';
 })
 
 rightAlign.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.justifyContent = 'right';
-    topTextMeme.style.justifyContent = 'right';
+    textSup.style.justifyContent = 'right';
+    textInf.style.justifyContent = 'right';
 })
 
 noneOut.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.webkitTextStrokeWidth = '0px';
-    topTextMeme.style.webkitTextStrokeWidth = '0px';
+    textSup.style.webkitTextStrokeWidth = '0px';
+    textInf.style.webkitTextStrokeWidth = '0px';
 })
 
 
 whiteOut.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.webkitTextStrokeWidth = '1px';
-    topTextMeme.style.webkitTextStrokeWidth = '1px';
-    bottomTextMeme.style.webkitTextStrokeColor = 'white';
-    topTextMeme.style.webkitTextStrokeColor = 'white';
+    textSup.style.webkitTextStrokeWidth = '1px';
+    textInf.style.webkitTextStrokeWidth = '1px';
+    textSup.style.webkitTextStrokeColor = 'white';
+    textInf.style.webkitTextStrokeColor = 'white';
 })
 
 blackOut.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.webkitTextStrokeWidth = '1px';
-    topTextMeme.style.webkitTextStrokeWidth = '1px';
-    bottomTextMeme.style.webkitTextStrokeColor = 'black';
-    topTextMeme.style.webkitTextStrokeColor = 'black';
+    textSup.style.webkitTextStrokeWidth = '1px';
+    textInf.style.webkitTextStrokeWidth = '1px';
+    textSup.style.webkitTextStrokeColor = 'black';
+    textInf.style.webkitTextStrokeColor = 'black';
 })
 
 fontColor.addEventListener ("change", (event)=>{
     nameColor.innerText = event.target.value;
-    bottomTextMeme.style.color = `${event.target.value}`;
-    topTextMeme.style.color = `${event.target.value}`;
+    textSup.style.color = `${event.target.value}`;
+    textInf.style.color = `${event.target.value}`;
 
 })
 
-fontColorBack.addEventListener ("change", (event)=>{
+textBackColor.addEventListener ("change", (event)=>{
     nameColorBack.innerText = event.target.value;
-    bottomTextMeme.style.backgroundColor = `${event.target.value}`;
-    topTextMeme.style.backgroundColor = `${event.target.value}`;
+    textSup.style.backgroundColor = `${event.target.value}`;
+    textInf.style.backgroundColor = `${event.target.value}`;
     
     console.log(event);    
 })
 
 
 spacing.addEventListener ("input", (event)=>{
-    bottomTextMeme.style.paddingTop = `${event.target.value}px`;
-    topTextMeme.style.paddingTop = `${event.target.value}px`;
-    bottomTextMeme.style.paddingBottom = `${event.target.value}px`;
-    topTextMeme.style.paddingBottom = `${event.target.value}px`;
+    textSup.style.paddingTop = `${event.target.value}px`;
+    textInf.style.paddingTop = `${event.target.value}px`;
+    textSup.style.paddingBottom = `${event.target.value}px`;
+    textInf.style.paddingBottom = `${event.target.value}px`;
 })
 
 lineSpacing.addEventListener ("click", (event)=>{
-    bottomTextMeme.style.lineHeight = `${event.target.value}px`;
-    topTextMeme.style.lineHeight = `${event.target.value}px`;
+    textSup.style.lineHeight = `${event.target.value}px`;
+    textInf.style.lineHeight = `${event.target.value}px`;
     
 })
 
 
 
 
-// TEXT EDITOR ASIDE
+// IMAGE EDITOR ASIDE
+
 
 // ------ Variables -------
 
 let inputUrl = $ ('#url');
 let imageContainer = $ ('.image-container');
+let brightnessInput = $ ('.brightness');
+let opacityInput = $ ('.opacity');
+let contrastInput = $ ('.contrast');
+let blurInput = $ ('.blur');
+let grayscaleInput = $ ('.grayscale');
+let sepiaInput = $ ('.sepia');
+let hueInput = $ ('.hue');
+let saturationInput = $ ('.saturation');
+let negativeInput = $ ('.negative');
+
+// ------ Eventos -------
 
 
 inputUrl.addEventListener ("input", (event)=>{
     imageContainer.style.backgroundImage = `url("${event.target.value}")`;
     
 })
-
-
-// EJERCICIO DE PRUEBA - FUNCIONA OK
-
-/* body.addEventListener("keydown", (event) => {
-    console.log(event);
-  
-    let code = event.code;
-  
-    if (code === "Space") {
-      alert("Saltaste");
-    }
-  
-    if (code === "Enter") {
-      alert("Aceptaste");
-    }
-})
- */
-
-
 
 // DESCARGAR MEME
 
@@ -270,3 +267,58 @@ const descargarMeme = () => {
 };
 
 btnDownload.addEventListener("click", descargarMeme);
+
+
+
+
+const updateAllFilters = () => {
+imageContainer.style.filter = `brightness(${brightnessInput.value}) opacity(${opacityInput.value}) contrast(${contrastInput.value}%) blur(${blurInput.value}px) grayscale(${grayscaleInput.value}%) sepia(${sepiaInput.value}%) hue-rotate(${hueInput.value}deg) saturate(${saturationInput.value}%) invert(${negativeInput.value})`
+
+};
+
+
+const reseatAllFilters = () => {
+
+}
+
+// Eventos Filtros
+
+
+brightnessInput.addEventListener ("change", (updateAllFilters));
+opacityInput.addEventListener ("change", (updateAllFilters));
+contrastInput.addEventListener ("change", (updateAllFilters));
+blurInput.addEventListener ("change", (updateAllFilters));
+grayscaleInput.addEventListener ("change", (updateAllFilters));
+sepiaInput.addEventListener ("change", (updateAllFilters));
+hueInput.addEventListener ("change", (updateAllFilters));
+saturationInput.addEventListener ("change", (updateAllFilters));
+negativeInput.addEventListener ("change", (updateAllFilters));
+
+
+
+
+
+
+/* function filtros () {
+    contenedorMeme.style.filter = `brightness(${brillo.value}) opacity(${opacidad.value}%) contrast(${contraste.value}%) blur(${desenfoque.value}px) grayscale(${escalaGrises.value}%) sepia(${sepia.value}%) hue-rotate(${hue.value}deg) saturate(${saturacion.value}%) invert(${negativo.value}))`;
+
+};
+
+
+
+
+const botonReestablecer = document.getElementById('botonReestablecer');
+
+botonReestablecer.addEventListener('click', ()=>{
+    brillo.value = 1;
+    opacidad.value = 1;
+    contraste.value = 100;
+    desenfoque.value = 0;
+    escalaGrises.value = 0;
+    sepia.value = 0;
+    hue.value = 0;
+    saturacion.value = 100;
+    negativo.value = 0;
+    filtros();
+});
+ */
