92c5b7012e904762736a6862e71f28ae3f7c46f5
Agusgoi <agus.goitea@live.com.ar>
2022-11-08 01:47:18 -0300

    responsive design 880px

--- main.js
+++ main.js
@@ -3,9 +3,8 @@
 const $ = (selector) => document.querySelector (selector);
 
 
-// CAMBIO DE MODE
 
-// ----- Variables -----
+// --------------- Variables ---------------
 
 let btnMode = $ ('.mode');
 let spanMode = $ ('.span-mode');
@@ -15,24 +14,71 @@ let asideText = $ ('.txt-aside');
 let asideImage = $ ('.img-aside');
 let btnText = $ ('.btn-text');
 let btnImage = $ ('.btn-image');
-let inputColors = $ ('.cont-color');
+let titles = $ ('h2');
+let subtitles = $ ('.subtitles');
+
+let topTextImput = $ ('.input-textarea1');
+let bottomTextImput = $ ('.input-textarea2');
+let textSup = $ ('.text-1');
+let textInf = $ ('.text-2');
+let contImage = $ ("#meme-container");
+let checkboxTop = $ ('#checkbox-top');
+let checkboxBottom = $ ('#checkbox-bottom');
+let checkboxBack = $ ('#checkbox-back');
+
+let fontSize = $ ('.font-size');
+let fontSelect = $ ('.input-font');
+let leftAlign = $ ('.btn-align1');
+let centerAlign = $ ('.btn-align2');
+let rightAlign = $ ('.btn-align3');
+let noneOut = $ ('.btn-outline1');
+let whiteOut = $ ('.btn-outline2');
+let blackOut = $ ('.btn-outline3');
+let fontColor = $ ('.input-color-font');
+let textBackColor = $ ('.input-color-back');
+let colorInputs = $ ('.cont-color');
+let nameColor = $ ('.name-color');
+let nameColorBack = $ ('.name-color-back');
+let spacing = $ ('.input-space');
+let lineSpacing = $ ('#line-spacing');
+let imageBackColor = $ ('.input-img-color');
+let nameColorImage = $ ('.name-color-image');
+
+let inputUrl = $ ('#url');
+let imageContainer = $ ('.image-container');
+let brightnessInput = $ ('.brightness');
+let opacityInput = $ ('.opacity');
+let contrastInput = $ ('.contrast');
+let blurInput = $ ('.blur');
+let grayscaleInput = $ ('.grayscale');
+let sepiaInput = $ ('.sepia');
+let hueInput = $ ('.hue');
+let saturationInput = $ ('.saturation');
+let negativeInput = $ ('.negative');
+let btnReset = $ ('.reset');
+let btnBlend = $ ('.input-img-colortype');
+
+let btnDownload = $ ("#download");
 
 
+// CAMBIO DE MODE ==============================================================
 
 
-// ----- Eventos -----
+// --------------- Funciones ---------------
 
 
 btnMode.addEventListener ("click", (event)=> {
     header.classList.toggle("darkmode-header");
     let estaModoOscuro = header.classList.contains("darkmode-header");
     if (estaModoOscuro) {
-    spanMode.innerText = "Modo Oscuro";
-    }else{
     spanMode.innerText = "Modo Claro";
+    }else{
+    spanMode.innerText = "Modo Oscuro";
 }
 })
 
+
+// --------------- Eventos ---------------
 
 btnMode.addEventListener ("click", (event)=> {
     btnMode.classList.toggle ("darkmode-header");
@@ -48,13 +94,19 @@ btnMode.addEventListener ("click", (event)=> {
     centerAlign.classList.toggle ("darkmode-header");
     rightAlign.classList.toggle ("darkmode-header");
     fontSize.classList.toggle ("darkmode-header");
-    inputColors.classList.toggle ("darkmode-header");
+    noneOut.classList.toggle ("darkmode-header");
+    blackOut.classList.toggle ("darkmode-header");
+    whiteOut.classList.toggle ("darkmode-header");
+    lineSpacing.classList.toggle ("darkmode-header");
+    spacing.classList.toggle ("darkmode-header");
+    titles.classList.toggle ("darkmode-aside");
+    subtitles.classList.toggle ("darkmode-aside");
+
+    // NO SE APLICA NI A LOS INPUT INDIV NI A LOS DIV
+    fontColor.classList.toggle ("darkmode-header");
+    textBackColor.classList.toggle ("darkmode-header");
+    colorInputs.classList.toggle ("darkmode-header"); 
 })
-
-
-
-
-//  Eventos de BTN TEXT & IMAGE
 
 
 btnText.addEventListener ("click", (event)=>{
@@ -73,46 +125,26 @@ btnImage.addEventListener ("click", (event)=>{
 
 
 
-// TEXT EDITOR ASIDE
-
-// ------ Variables -------
-
-let body = document.body;
+// TEXT EDITOR ASIDE =========================================================
 
 
-let topTextImput = $ ('.input-textarea1');
-let bottomTextImput = $ ('.input-textarea2');
-let textSup = $ ('.text-1');
-let textInf = $ ('.text-2');
 
-let checkboxTop = $ ('#checkbox-top');
-let checkboxBottom = $ ('#checkbox-bottom');
-let checkboxBack = $ ('#checkbox-back');
+// --------------- Funciones ---------------
 
-let fontSize = $ ('.font-size');
-let fontSelect = $ ('.input-font');
+checkboxBack.addEventListener ("change", (event)=>{
+  
+    if (event.target.checked) {
+       textInf.style.backgroundColor = 'transparent';
+       textSup.style.backgroundColor = 'transparent';
+    }else{
+       textInf.style.backgroundColor = fontColorBack.value;
+       textSup.style.backgroundColor = fontColorBack.value;
+    }
+   
+   })
+   
 
-let leftAlign = $ ('.btn-align1');
-let centerAlign = $ ('.btn-align2');
-let rightAlign = $ ('.btn-align3');
-
-let noneOut = $ ('.btn-outline1');
-let whiteOut = $ ('.btn-outline2');
-let blackOut = $ ('.btn-outline3');
-
-let fontColor = $ ('.input-color-font');
-let textBackColor = $ ('.input-color-back');
-let nameColor = $ ('.name-color');
-let nameColorBack = $ ('.name-color-back');
-let spacing = $ ('.input-space');
-let lineSpacing = $ ('#line-spacing');
-let imageBackColor = $ ('.input-img-color');
-let nameColorImage = $ ('.name-color-image');
-
-let contImage = $ ("#meme-container");
-
-
-// ----- Eventos ------
+// --------------- Eventos ---------------
 
 
 bottomTextImput.addEventListener ("input", (event)=>{
@@ -135,22 +167,6 @@ checkboxBottom.addEventListener ("click", (event)=>{
     textInf.classList.toggle ("display-none");
     console.log (event);
 })
-
-
-
-checkboxBack.addEventListener ("change", (event)=>{
-  
- if (event.target.checked) {
-    textInf.style.backgroundColor = 'transparent';
-    textSup.style.backgroundColor = 'transparent';
- }else{
-    textInf.style.backgroundColor = fontColorBack.value;
-    textSup.style.backgroundColor = fontColorBack.value;
- }
-
-})
-
-
 
 fontSize.addEventListener ("input", (event)=>{
     textSup.style.fontSize = `${event.target.value}px`;
@@ -230,65 +246,17 @@ lineSpacing.addEventListener ("click", (event)=>{
 
 
 
-// IMAGE EDITOR ASIDE
-
-
-// ------ Variables -------
-
-let inputUrl = $ ('#url');
-let imageContainer = $ ('.image-container');
-let brightnessInput = $ ('.brightness');
-let opacityInput = $ ('.opacity');
-let contrastInput = $ ('.contrast');
-let blurInput = $ ('.blur');
-let grayscaleInput = $ ('.grayscale');
-let sepiaInput = $ ('.sepia');
-let hueInput = $ ('.hue');
-let saturationInput = $ ('.saturation');
-let negativeInput = $ ('.negative');
-let btnReset = $ ('.reset');
-
-let btnBlend = $ ('.input-img-colortype');
+// IMAGE EDITOR ASIDE ========================================================
 
 
 
 
-// ------ Eventos -------
-
-
-inputUrl.addEventListener ("input", (event)=>{
-    imageContainer.style.backgroundImage = `url("${event.target.value}")`;
-    
-})
-
-
-imageBackColor.addEventListener ("change", (event)=>{
-    nameColorImage.innerText = event.target.value;
-    imageContainer.style.backgroundColor = `${event.target.value}`; 
-
-})
-
-btnBlend.addEventListener ("change", (event)=>{
-    
-    imageContainer.style.backgroundBlendMode = `${event.target.value}`;
-})
-
-
-// ------ filtros -------
+// --------------- Funciones ---------------
 
 
 const updateAllFilters = () => {
 imageContainer.style.filter = `brightness(${brightnessInput.value}) opacity(${opacityInput.value}) contrast(${contrastInput.value}%) blur(${blurInput.value}px) grayscale(${grayscaleInput.value}%) sepia(${sepiaInput.value}%) hue-rotate(${hueInput.value}deg) saturate(${saturationInput.value}%) invert(${negativeInput.value}) `
 
-/* console.log (brightnessInput.value)
-console.log (opacityInput.value)
-console.log (contrastInput.value)
-console.log (blurInput.value)
-console.log (grayscaleInput.value)
-console.log (sepiaInput.value)
-console.log (hueInput.value)
-console.log (saturationInput.value)
-console.log (negativeInput.value) */
 };
 
 
@@ -306,7 +274,9 @@ const reseatAllFilters = () => {
     updateAllFilters (); // para que el reset de los filtros se aplique en la imagen.
 }
  
-// Eventos Filtros
+
+
+// --------------- Eventos ---------------
 
 
 brightnessInput.addEventListener ("change", (updateAllFilters));
@@ -322,18 +292,37 @@ btnReset.addEventListener ("click", (reseatAllFilters));
 
 
 
+inputUrl.addEventListener ("input", (event)=>{
+    imageContainer.style.backgroundImage = `url("${event.target.value}")`;
+    
+})
+
+imageBackColor.addEventListener ("change", (event)=>{
+    nameColorImage.innerText = event.target.value;
+    imageContainer.style.backgroundColor = `${event.target.value}`; 
+
+})
+
+btnBlend.addEventListener ("change", (event)=>{
+    imageContainer.style.backgroundBlendMode = `${event.target.value}`;
+})
 
 
-// DESCARGAR MEME
+// DESCARGAR MEME ====================================================
 
 
-let btnDownload = $ ("#download");
+
+// --------------- Funcion ---------------
 
 const descargarMeme = () => {
   domtoimage.toBlob(contImage).then(function (blob) {
     saveAs(blob, "mi-meme.jpg");
   });
 };
+
+
+// --------------- Evento ---------------
+
 
 btnDownload.addEventListener("click", descargarMeme);
 
